738dedb67fb6b4610fe94f321dcabc7328cf76ce
Sean Shadle <seandshadle@gmail.com>
2020-09-14 17:56:28 -0500

    Completed MVP starting on stretch

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 
 export default function SavedList(props) {
   return (
+    <Link to={`/`}>
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
@@ -9,5 +11,6 @@ export default function SavedList(props) {
       ))}
       <div className="home-button">Home</div>
     </div>
+    </Link>
   );
 }
