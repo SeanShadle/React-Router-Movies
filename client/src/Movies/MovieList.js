900b3f41ee8b4353023fbd522aa5b6bdad06f2cf
Sean Shadle <seandshadle@gmail.com>
2020-09-14 22:23:51 -0500

    Completed stretch

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from "react-router-dom"
+import MovieCard from "./MovieCard"
 
 export default function MovieList(props) {
   return (
@@ -12,21 +13,9 @@ export default function MovieList(props) {
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
   return (
     <Link to={`/movies/${props.movie.id}`}>
-    <div className="movie-card">
-      <h2>{title}</h2>
-
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
+      <MovieCard {...props.movie}/>
     </Link>
   );
 }
